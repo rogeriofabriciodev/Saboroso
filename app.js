ce710d649aa808a66cde0c93c2f9f790539ac3d1
Rogério Fabrício <60872513+rogeriofabriciodev@users.noreply.github.com>
2021-01-28 20:12:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-http.listen(process.env.PORT, function() {
+http.listen(port, function() {
 
   console.log('servidor online');
 
